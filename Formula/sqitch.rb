21925e8ab9142d558fdc52ef84ffb24c64de31f4
David E. Wheeler <david@justatheory.com>
2014-11-05 09:11:40 -0800

    Fix typo, it is 0.997, not 0.998.
    Thanks @ehamberg!

--- Formula/sqitch.rb
+++ Formula/sqitch.rb
@@ -14,7 +14,7 @@ class Sqitch < Formula
   end
 
   homepage   'http://sqitch.org/'
-  version    '0.998'
+  version    '0.997'
   url        "http://cpan.cpantesters.org/authors/id/D/DW/DWHEELER/App-Sqitch-#{stable.version}.tar.gz"
   sha1       'ad0f64d99da1bd14ac80c5f8dee48b950021bb30'
   head       'https://github.com/theory/sqitch.git'
